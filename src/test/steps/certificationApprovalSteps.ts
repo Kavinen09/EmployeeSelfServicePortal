1526685ee273d4b9f181736a6f17f9307d26aa3b
Kavinen09 <kavinen.thanjappan@sdworx.com>
2025-03-23 10:45:30 +0400

    Negative Scenario Completed

--- src/test/steps/certificationApprovalSteps.ts
+++ src/test/steps/certificationApprovalSteps.ts
@@ -46,6 +46,16 @@ Then('the employee should see the file name of the uploaded certificate displaye
   await certificationApprovalpage.verifyFileNameDisplayed('certificationGP.jpg');
 });
 
+When('the employee uploads a valid certificate', async function () {
+  // Upload a certificate from the specified path
+  await certificationApprovalpage.uploadValidCertificate('downloads/negativeGP.jpg');
+});
+
+Then('the employee should see the valid file name of the uploaded certificate displayed', async function () {
+  //verify the file name is displayed
+  await certificationApprovalpage.verifyValidFileNameDisplayed('negativeGP.jpg');
+});
+
 Then('the employee should see the request recorded message', async function () {
   // Verify that the request was recorded and validated
   await certificationApprovalpage.verifyRequestRecorded();
@@ -94,6 +104,11 @@ When('the manager types in the comment box with the text {string}', async (comme
   await certificationApprovalpage.fillCommentBox(commentText);
 });
 
+When('the manager leaves the comment box empty', async () => {
+  await certificationApprovalpage.clearCommentBox();  // Clear the comment box to leave it empty
+});
+
+
 When('the manager clicks the Approve button', async () => {
   // Click the Approve button
   await certificationApprovalpage.clickApproveButton();
@@ -102,6 +117,18 @@ When('the manager clicks the Approve button', async () => {
 Then('the request should be approved successfully', async () => {
   // Verify if the success message is shown
   await certificationApprovalpage.verifyApprovalSuccess();
+});
+
+// Manager clicks on the "Decline" button for a task
+When('the manager clicks the Decline button', async () => {
+  // Use the method from your POM to click on the Decline button
+  await certificationApprovalpage.clickDeclineButton();
+});
+
+// Manager should see a success message indicating the request was declined
+Then('the request should be declined successfully', async () => {
+  // Verify if the decline success message is displayed
+  await certificationApprovalpage.verifyDeclineSuccess();
 });
 
 //Manager signs out
@@ -151,6 +178,17 @@ Then('the HR user should see the notification with the message {string}', async 
     await certificationApprovalpage.verifyNotificationBody(message); // Verify that the message matches the expected
 });
 
+// Step to check if HR sees the notification title for the decline scenario
+Then('the HR user should see the decline notification with the title {string}', async (title: string) => {
+  await certificationApprovalpage.isNegativeNotificationWithTitleVisible(title); // Verify that the title matches the expected
+});
+
+// Step to check if HR sees the notification message for the decline scenario
+Then('the HR user should see the decline notification with the message {string}', async (message: string) => {
+  await certificationApprovalpage.verifyNegativeNotificationBody(message); // Verify that the message matches the expected
+});
+
+
 When('the HR user clicks the sign-out button', async function () {
   await certificationApprovalpage.clickHrSignOutButton();  // Call the method to click sign out
 });
@@ -176,6 +214,21 @@ Then('the employee should see the notification with the message {string}', async
     await certificationApprovalpage.verifyEmployeeNotificationBody(message); // Verify that the message matches the expected
 });
 
+
+Then('the employee should see the decline notification with the title {string}', async (title: string) => {
+  await certificationApprovalpage.isEmployeeNegativeNotificationWithTitleVisible(title); // Verify that the title matches the expected
+
+});
+
+Then('the employee should see the decline notification with the message {string}', async (message: string) => {
+    await certificationApprovalpage.verifyNegativeEmployeeNotificationBody(message); // Verify that the message matches the expected
+});
+
+
 Then('the employee should see certificate is present', async function () {
   await certificationApprovalpage.verifyCertificationIsPresent();
+});
+
+Then('the employee should see {string} certificate is not present', async function (imageName: string) {
+  await certificationApprovalpage.verifyCertificationIsNotPresent(imageName);
 });
